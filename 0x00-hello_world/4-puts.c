ea279e1d4e21d7e2d5fdadff23de691fb001cdbe
mycrochip <michaelwahabtobi@gmail.com>
2022-09-08 21:34:49 -0700

    Added 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	//String initialization
+	char message[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(message);
+
+	return 0;
+}

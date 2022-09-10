581418918b143fcefa08a394853f559e35ac2a74
mycrochip <michaelwahabtobi@gmail.com>
2022-09-09 21:52:18 -0700

    Updated 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,10 @@ int main(void)
 	int i, j;
 
 	for (i = 0; i <= 9; i++)
-		for (j = 0; j <= 9; j++)
+		if (i != 9)
 		{
 			putchar(i + '0');
-			putchar(j + ',');
+			putchar(',');
 			putchar(' ');
 		}
 	putchar('\n');

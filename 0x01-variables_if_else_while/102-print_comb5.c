0e2a021027d9006e32fd0ecc25c246d928dd5109
mycrochip <michaelwahabtobi@gmail.com>
2022-09-09 23:25:02 -0700

    Added 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int tens1, tens2, unit1, unit2;
+
+	for (tens1 = 0; tens1 <= 9; tens1++)
+	{
+		/* tens2 is dependent on tens1 as such */
+		tens2 = tens1;
+		for (tens2; tens2 <= 9; tens2++)
+		{
+
+			for (unit1 = 1; unit1 <= 9; unit1++)
+			{
+				/* unit2 is dependent on unit1 as such */
+				unit2 = unit1 + 1;
+				for (unit2; unit2 <= 9; unit2++)
+				{
+					putchar(tens1 + '0');
+					putchar(unit1 + '0');
+
+					putchar(' ');
+
+					putchar(tens2 + '0');
+					putchar(unit2 + '0');
+					putchar(',');
+
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}

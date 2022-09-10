2efa805aa285cf03f7b2b80f7e19c16331456f7a
mycrochip <michaelwahabtobi@gmail.com>
2022-09-09 20:16:31 -0700

    Added 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,6 +2,7 @@
 #include <time.h>
 /* more headers goes there */
 #include <stdio.h>
+#include <string.h>
 /* betty style doc for function main goes there */
 /**
  * main - Entry point
@@ -12,13 +13,13 @@ int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	char last_digit;
 
 	char message[30] = "";
 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
 	last_digit = n % 10;
 
 	if (last_digit == 0)

5b78388e722ca20f19abb2e722c6a76115805886
mycrochip <michaelwahabtobi@gmail.com>
2022-09-09 21:46:25 -0700

    Added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+		for (j = 0; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + ',');
+			putchar(' ');
+	putchar('\n');
+	return (0);
+}

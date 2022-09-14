a892f929a716a37e58ce34b7e58d9f98ad582099
mycrochip <michaelwahabtobi@gmail.com>
2022-09-13 20:08:12 -0700

    Updated 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,6 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	print_alphabet();
-
-	return (0);
-}
-
-/**
  * print_alphabet - used to output alphabets in lowercase
  *
  *Return: void

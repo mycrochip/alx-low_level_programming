a2760e5d7fa4430f173ee854075656e508d6a90c
mycrochip <michaelwahabtobi@gmail.com>
2022-09-15 19:21:43 -0700

    Added main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _isupper(int);
+
+int _isdigit(int);
+
+int mul(int, int);
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int);
+
+void print_diagonal(int);
+
+void print_square(int);
+
+#endif /* MAIN_H */

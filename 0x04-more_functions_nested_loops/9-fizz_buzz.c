f193c06ca1be5d72d5429a1f9fd5ec78b843a700
mycrochip <michaelwahabtobi@gmail.com>
2022-09-16 09:24:11 -0700

    Updated 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,19 +10,16 @@ int main(void)
 	int i;
 
 	for (i = 1; i <= 100; i++)
+	{
 		if (i % 3 == 0)
-		{
 			printf("Fizz");
-		}
 		if (i % 5 == 0)
-		{
 			printf("Buzz");
-		}
-		if !((i % 3 == 0) && (i % 5 == 0))
-		{
+		if (!(i % 3 == 0) && !(i % 5 == 0))
 			printf("%d", i);
-		}
 		printf(" ");
+	}
 	printf("\n");
+
 	return (0);
 }

de7a0788a01cc3150278ca9d5b1653ad744084b5
mycrochip <michaelwahabtobi@gmail.com>
2022-09-22 11:27:04 -0700

    Added 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings.
+ * Adding the source string to the
+ *  destination string.
+ * @dest: The destination string.
+ * @src: The source string.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}

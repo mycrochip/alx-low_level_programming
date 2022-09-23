f5f989c06a793ae56624811289f5235d8c29e8ed
mycrochip <michaelwahabtobi@gmail.com>
2022-09-22 21:25:17 -0700

    Updated 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,11 +17,11 @@ int _strcmp(char *s1, char *s2)
 
 	while (*first && *second)
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		s1++;
-		s2++;
+		if (*first != *second)
+			return (*first - *second);
+		first++;
+		second++;
 	}
 
-	return (*s1 - *s2);
+	return (*first - *second);
 }

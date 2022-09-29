21f2aea41c7f4eaea35d63cda76bf21c7062c507
mycrochip <michaelwahabtobi@gmail.com>
2022-09-28 22:21:50 -0700

    Updated 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -20,7 +20,7 @@ int _sqrt_recursion(int n)
  * using binary search.
  * @start: Initial number.
  * @end: Limit of number.
- * @num: Input number.
+ * @num: Input number to compare.
  * Return: 1 if not found, else square root.
  */
 int sqrtRecursion(int start, int end, int num)
@@ -31,12 +31,12 @@ int sqrtRecursion(int start, int end, int num)
 	{
 		mid = start + (end - start) / 2;
 		if (mid * mid == num)
-			return (mid);
-/* following binary search */
+			return (mid); /* (*)Terminate here... */
+		/* Binary search */
 		if (mid * mid > num)
 			return (sqrtRecursion(start, mid - 1, num));
 		if (mid * mid < num)
 			return (sqrtRecursion(mid + 1, end, num));
 	}
-	return (-1);
+	return (-1); /* (*)...or here */
 }

707b1c247b1e60578eb83d3e8b36f6bf02b31cdd
mycrochip <michaelwahabtobi@gmail.com>
2022-09-28 21:22:41 -0700

    Added 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Finds the length of a string.
+ * @s: Pointer to the string.
+ * Return: Length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursions(s + 1));
+}

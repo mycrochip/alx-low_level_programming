a4ee051d54e573044f2be7fbcb93ef56c0c53466
mycrochip <michaelwahabtobi@gmail.com>
2022-09-30 21:12:35 -0700

    Modified 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -16,3 +16,19 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
+
+/**
+ * _prints - Prints a string to sdout.
+ * @ptr: A pointer to the string.
+ * Return: Nothing.
+ */
+void _prints(char *ptr)
+{
+	/* Output a string */
+	while (*ptr)
+	{
+		_putchar(*ptr);
+		ptr++;
+	}
+	_putchar('\n');
+}

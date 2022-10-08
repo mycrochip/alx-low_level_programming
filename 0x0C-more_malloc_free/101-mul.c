f87c6fe976980359c6dd6079698a375985f9fa11
mycrochip <michaelwahabtobi@gmail.com>
2022-10-07 21:01:34 -0700

    Added 101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,93 @@
+#include "main"
+#include <stdlib.h>
+
+void _prints(char *ptr);
+
+void _printn(int *ptr);
+
+/**
+ * main - Check the code
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: Nothing
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		_prints("Error");
+		exit(98);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+
+		_printn(result);
+
+		return (0);
+	}
+}
+
+/**
+ * _prints - Prints a string to sdout.
+ * @ptr: A pointer to the string.
+ * Return: Nothing.
+ */
+void _prints(char *ptr)
+{
+	/* Output a string */
+	while (*ptr)
+	{
+		_putchar(*ptr);
+		ptr++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * _printn - Prints an integer to sdout.
+ * @ptr: A pointer to the integer.
+ * Return: Nothing.
+ */
+void _printn(int *ptr)
+{
+	int val, place;
+	int num, absval;
+	int neg;
+
+	neg = 0;
+	val = absval = *ptr;
+	if (*ptr < 0)
+	{
+		neg = 1;
+		absval = -val;
+	}
+
+	/* Find the place value */
+	place = 1;
+	val = absval
+	while (val >= 10)
+	{
+		place *= 10;
+		val /= 10;
+	}
+
+	/* Output each value from the highest */
+	if (neg)
+		_putchar('-');
+
+	val = absval;
+	while (place >= 1)
+	{
+		num = val / place;
+		_putchar(num + '0');
+
+		val -= (num * place);
+		place /= 10;
+	}
+	_putchar('\n');
+}
---
+++ 0x0C-more_malloc_free/temp/_printer.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * _prints - Prints a string to sdout.
+ * @ptr: A pointer to the string.
+ * Return: Nothing.
+ */
+void _prints(char *ptr)
+{
+	/* Output a string */
+	while (*ptr)
+	{
+		_putchar(*ptr);
+		ptr++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * _printn - Prints an integer to sdout.
+ * @ptr: A pointer to the integer.
+ * Return: Nothing.
+ */
+void _printn(int *ptr)
+{
+	int val, place;
+	int num, absval;
+	int neg;
+
+	neg = 0;
+	val = absval = *ptr;
+	if (*ptr < 0)
+	{
+		neg = 1;
+		absval = -val;
+	}
+
+	/* Find the place value */
+	place = 1;
+	val = absval
+	while (val >= 10)
+	{
+		place *= 10;
+		val /= 10;
+	}
+
+	/* Output each value from the highest */
+	if (neg)
+		_putchar('-');
+
+	val = absval;
+	while (place >= 1)
+	{
+		num = val / place;
+		_putchar(num + '0');
+
+		val -= (num * place);
+		place /= 10;
+	}
+	_putchar('\n');
+}

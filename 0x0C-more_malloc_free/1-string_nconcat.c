6db6450340f06895afac8cf1b845a60acf28335f
mycrochip <michaelwahabtobi@gmail.com>
2022-10-07 18:46:11 -0700

    Added 1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes of s2 to use
+ * Return: pointer to new space in memory or null
+ **/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	int i;
+	unsigned int j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	if (j > n)
+		n = j;
+	str = malloc_checked(sizeof(char) * (i + n + 1));
+	if (str == NULL)
+		return (NULL);
+	i = j = 0;
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (j < n && s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		i++, j++;
+	}
+	str[i] = '\0';
+	return (str);
+}

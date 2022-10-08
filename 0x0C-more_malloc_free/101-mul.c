5c515ecae59e1730aabfc7d12638275d6003e24a
mycrochip <michaelwahabtobi@gmail.com>
2022-10-07 21:44:45 -0700

    Updated 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,9 +1,5 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-
-void _prints(char *ptr);
-
-void _printn(long int *ptr);
 
 /**
  * main - Check the code
@@ -13,11 +9,11 @@ void _printn(long int *ptr);
  */
 int main(int argc, char **argv)
 {
-	long int num1, num2, result;
+	int num1, num2, result;
 
 	if (argc != 3)
 	{
-		_prints("Error");
+		printf("Error\n");
 		exit(98);
 	}
 	else
@@ -25,69 +21,7 @@ int main(int argc, char **argv)
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
 		result = num1 * num2;
-
-		_printn(&result);
-
+		printf("%d\n", result);
 		return (0);
 	}
-}
-
-/**
- * _prints - Prints a string to sdout.
- * @ptr: A pointer to the string.
- * Return: Nothing.
- */
-void _prints(char *ptr)
-{
-	/* Output a string */
-	while (*ptr)
-	{
-		_putchar(*ptr);
-		ptr++;
-	}
-	_putchar('\n');
-}
-
-/**
- * _printn - Prints an integer to sdout.
- * @ptr: A pointer to the integer.
- * Return: Nothing.
- */
-void _printn(long int *ptr)
-{
-	long int val, place;
-	long int num, absval;
-	int neg;
-
-	neg = 0;
-	val = absval = *ptr;
-	if (*ptr < 0)
-	{
-		neg = 1;
-		absval = -val;
-	}
-
-	/* Find the place value */
-	place = 1;
-	val = absval;
-	while (val >= 10)
-	{
-		place *= 10;
-		val /= 10;
-	}
-
-	/* Output each value from the highest */
-	if (neg)
-		_putchar('-');
-
-	val = absval;
-	while (place >= 1)
-	{
-		num = val / place;
-		_putchar(num + '0');
-
-		val -= (num * place);
-		place /= 10;
-	}
-	_putchar('\n');
 }

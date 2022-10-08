3b9902b57747abab0777a6f1b288cd6dee6c463b
mycrochip <michaelwahabtobi@gmail.com>
2022-10-07 20:26:17 -0700

    Added 100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: old size in bytes of memory allocated space
+ * @new_size: new size in bytes of memory to be allocated
+ * Return: void pointer to new memory location
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new_loc, *new_ptr;
+	unsigned int i = 0;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	memory = malloc(new_size);
+	new_ptr = ptr;
+	if (new_size > old_size)
+		new_size = old_size;
+
+	for (i = 0; i < new_size; i++)
+		memory[i] = new_ptr[i];
+	free(ptr);
+
+	return (memory);
+}

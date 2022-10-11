4a1eef9449a6b6416e83a884a1ad9637a4843611
mycrochip <michaelwahabtobi@gmail.com>
2022-10-11 06:18:43 -0700

    Added 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+# include "main.h"
+
+/**
+ * init_dog - Initializes a variable of type struct dog.
+ *@d: A pointer to the dog struct.
+ *@name: A pointer to the variable identifier.
+ *@age: The variable age attribute.
+ *@owner: A pointer to the variable owner identifier.
+ *Return: Nothing.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}

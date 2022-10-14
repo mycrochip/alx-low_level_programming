b923ecf6b62b78670ffac69308360be7bc914666
mycrochip <michaelwahabtobi@gmail.com>
2022-10-13 21:17:19 -0700

    Updated 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -45,7 +45,7 @@ int op_div(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Error: Division by zero.\n");
+		printf("Error\n");
 		exit(100);
 	}
 
@@ -62,7 +62,7 @@ int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Error: Division by zero.\n");
+		printf("Error\n");
 		exit(100);
 	}
 	return (a % b);

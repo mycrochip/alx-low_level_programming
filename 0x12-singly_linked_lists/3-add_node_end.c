f0b32514370eabd90deda9c28cbdc8691a0041cb
mycrochip <michaelwahabtobi@gmail.com>
2022-10-21 20:42:30 -0700

    Updated 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int _strlen(const char *s);
+
+void *_strdup(const char *src);
 
 /**
  * add_node_end - A function that adds a node to the end of linked list
@@ -10,17 +15,15 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *current;
-	int _strlen = 0;
+	int length = 0;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	while (str[_strlen])
-		_strlen++;
-
-	new_node->len = _strlen;
-	new_node->str = strdup(str);
+	length = _strlen(str);
+	new_node->len = length;
+	new_node->str = _strdup(str);
 	new_node->next = NULL;
 	if (*head == NULL)
 		*head = new_node;
@@ -32,4 +35,38 @@ list_t *add_node_end(list_t **head, const char *str)
 		current->next = new_node;
 	}
 	return (*head);
+}
+
+/**
+ * _strlen - gets length of the string
+ * @s: string
+ * Return: length of the string
+ */
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * _strdup - recreation of string duplicate function
+ * @src: source of string to duplicate
+ * Return: pointer to malloc'd space with copied string
+ */
+void *_strdup(const char *src)
+{
+	int len, i;
+	char *dest;
+
+	len = _strlen(src);
+	dest = malloc((len + 1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
 }

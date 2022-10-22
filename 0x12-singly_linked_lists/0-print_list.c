7197e1bc1082449f3a4941170c82b72aaef71507
mycrochip <michaelwahabtobi@gmail.com>
2022-10-21 20:55:24 -0700

    Updated 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
 #include <stdlib.h>
+
+#ifndef _PRINT_LIST
+#define _PRINT_LIST
+#define _FMT(x) ((x) ? (x) : "(nil)")
+#endif /* _PRINT_LIST */
+
 /**
  * print_list - Prints all elements of a list_t list
  * @h: Pointer to the singly-linked list
@@ -12,7 +18,7 @@ size_t print_list(const list_t *h)
 
 	for (i = 0; h; i++)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		printf("[%d] %s\n", h->len, h->_FMT(str));
 		h = h->next;
 	}
 	return (i);

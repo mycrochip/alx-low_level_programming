8ce17202c5d315f5a6b2a78253112b8d749baace
mycrochip <michaelwahabtobi@gmail.com>
2022-10-24 10:49:08 -0700

    Added 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - Sum all arguments
+ * @n: Number of arguments
+ * Return: 0 is n is 0 or return sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list numbers;
+	int sum;
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+
+	sum = 0;
+	va_start(numbers, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(numbers, int);
+	va_end(numbers);
+
+	return (sum);
+}

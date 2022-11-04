9c44bfba7cdfea1ca010bf0a6c83f206514372ca
mycrochip <michaelwahabtobi@gmail.com>
2022-11-03 20:26:33 -0700

    Added 6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * pop_listint - A function that deletes the first node
+ * of a linked listint_t list
+ * @head: A pointer to the head pointer of the list
+ * Return: Data from the deleted head
+ */
+int pop_listint(listint_t **head)
+{
+	int data;
+	listint_t *current, *tmp;
+
+	if (*head == NULL)
+		return (0);
+	tmp = current = *head;
+	if (*head)
+	{
+		data = current->n;
+		*head = current->next;
+		free(tmp);
+	}
+	else
+		i = 0;
+	return (i);
+}

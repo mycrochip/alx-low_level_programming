005ddfe85bd5b25a21b498d43c63ed491988b912
mycrochip <michaelwahabtobi@gmail.com>
2022-11-03 21:01:44 -0700

    Updated 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,10 +10,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t n_nodes;
 
-	for (n_nodes = 0; h; n_nodes++)
+	n_nodes = 0;
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
+		n_nodes++;
 	}
 	return (n_nodes);
 }

740f4f94994b56ee163f7ae58076040f19279914
mycrochip <michaelwahabtobi@gmail.com>
2022-11-03 20:26:05 -0700

    Added 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - Frees a linked listint_t list
+ * @head: Double pointer to the first node
+ */
+void free_listint2(listint_t **head)
+{
+	if (*head == NULL)
+		return;
+	free_listint(*head);
+	free(*head);
+}
+
+/**
+ * free_listint - Frees a linked listint_t list
+ * @head: Pointer to the first node
+ */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+	free_listint(head->next);
+	free(head);
+}

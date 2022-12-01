5683ab1cbbdc72ce229a8e9bb7ee6f88b4513bf5
mycrochip <michaelwahabtobi@gmail.com>
2022-12-01 03:02:29 -0800

    Updated 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * set_bit - Function that sets the bit of a number
- * at a given index to 1
- * @n: Pointer to the number
- * @index: The given index
+ * set_bit - Function that sets the value of a number's bit
+ * at a given index to 1.
+ * @n: Pointer to the number.
+ * @index: The given index.
  * Return: 1 on success, otherwise -1.
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > (8 * sizeof(*n)) - 1)
-		return (-1);
 	*n = *n | (1 << index);
-	return (1);
+	return ((index > 8 * sizeof(*n)) ? (-1) : (1));
 }

8ffe1517b4551d1d35bd0650065b05b0b53831d4
mycrochip <michaelwahabtobi@gmail.com>
2022-12-05 00:53:36 -0800

    Added 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		length = 0;
 		while (*(text_content + length) != '\0')
-			lenght++;
+			length++;
 		n_write = write(fd, text_content, length);
 		if (n_write == -1)
 		{

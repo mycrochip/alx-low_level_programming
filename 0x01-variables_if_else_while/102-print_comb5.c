abe7f355b1aafc5b5d9d9d5a9f69e669d21fea19
Wahab Michael <65517681+mycrochip@users.noreply.github.com>
2023-07-14 16:46:37 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -22,7 +22,7 @@ int main(void)
 				unit2 = unit1;
 				for (; unit2 <= 9; unit2++)
 				{
-					if (tens1==tens2 && unit1==unit2 && tens1==0 && unit1==0)
+					if (tens1==0 && tens2==0 && unit1==0 && unit2==0)
 						continue;
 					putchar(tens1 + '0');
 					putchar(unit1 + '0');

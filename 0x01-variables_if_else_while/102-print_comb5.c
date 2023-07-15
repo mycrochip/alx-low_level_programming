f0949170b01cd4de1dd2d18fedfa2c2562089218
mycrochip <michaelwahabtobi@gmail.com>
2023-07-15 03:30:09 -0700

    Corrected whitespaces for 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,32 +7,32 @@
  */
 int main(void)
 {
-        int digit1, digit2;
-        int unit1, unit2, tens1, tens2;
+	int digit1, digit2;
+	int unit1, unit2, tens1, tens2;
 
-        for (digit1 = 0; digit1 <= 99; digit1++)
-        {
-                tens1 = digit1 / 10;
-                unit1 = digit1 % 10;
-                for (digit2 = digit1 + 1; digit2 <= 99; digit2++)
-                {
-                        tens2 = digit2 / 10;
-                        unit2 = digit2 % 10;
+	for (digit1 = 0; digit1 <= 99; digit1++)
+	{
+		tens1 = digit1 / 10;
+		unit1 = digit1 % 10;
+		for (digit2 = digit1 + 1; digit2 <= 99; digit2++)
+		{
+			tens2 = digit2 / 10;
+			unit2 = digit2 % 10;
 
-                        putchar(tens1 + '0');
-                        putchar(unit1 + '0');
+			putchar(tens1 + '0');
+			putchar(unit1 + '0');
 
-                        putchar(' ');
-                        putchar(tens2 + '0');
-                        putchar(unit2 + '0');
+			putchar(' ');
+			putchar(tens2 + '0');
+			putchar(unit2 + '0');
 
-                        if (digit1 + 1 == 99 && digit2 == 99)
-                                break;
+			if (digit1 + 1 == 99 && digit2 == 99)
+				break;
 
-                        putchar(',');
-                        putchar(' ');
-                }
-        }
-        putchar('\n');
-        return (0);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }

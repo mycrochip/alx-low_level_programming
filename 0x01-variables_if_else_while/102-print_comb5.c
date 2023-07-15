e2dfe4f02b24daaf0127650926e2fd8153b0f0c3
Wahab Michael <65517681+mycrochip@users.noreply.github.com>
2023-07-15 09:53:22 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,13 +11,13 @@ int main(void)
 
 	for (tens1 = 0; tens1 <= 9; tens1++)
 	{
-		/* tens2 is dependent on tens1 as such */
-		tens2 = tens1;
 		for (unit1 = 0; unit1 <= 9; unit1++)
 		{
 
 			for (; tens2 <= 9; tens2++)
 			{
+				/* tens2 is dependent on tens1 as such */
+				tens2 = tens1;
 				/* unit2 is dependent on unit1 as such */
 				unit2 = unit1;
 				if (unit1==unit2 && tens1==tens2)

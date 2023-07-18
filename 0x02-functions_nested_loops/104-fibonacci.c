c4bf710b817cc0a2e9ef9c3dc15061cab9719ce8
Wahab Michael <65517681+mycrochip@users.noreply.github.com>
2023-07-18 14:25:36 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,20 +10,20 @@ int main(void)
 {
 	int i;
 
-	ino_t fibonacci[98];
+	uintmax_t fibonacci[98];
 
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
 
-	printf("%ju, %ju, ", (uintmax_t)fibonacci[0], (uintmax_t)fibonacci[1]);
+	printf("%ju, %ju, ", fibonacci[0], fibonacci[1]);
 
 	for (i = 2; i < 98; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 		if (i == 97)
-			printf("%ju\n", (uintmax_t)fibonacci[i]);
+			printf("%ju\n", fibonacci[i]);
 		else
-			printf("%ju, ", (uintmax_t)fibonacci[i]);
+			printf("%ju, ", fibonacci[i]);
 	}
 
 	return (0);

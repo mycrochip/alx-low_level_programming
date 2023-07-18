c51ce8bf9e9ad2ea532a83f555a39ec0b8878e27
Wahab Michael <65517681+mycrochip@users.noreply.github.com>
2023-07-18 13:55:24 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,16 +11,21 @@ void times_table(void)
 
 	for (row = 0; row <= 9; row++)
 	{
-		for (col = 0; col <= 9; col++)
+		_putchar(48);
+		for (col = 1; col <= 9; col++)
 		{
 			val = row * col;
-			_putchar((val / 10) ? ((val / 10) + '0') : 0); /* ASCII 0 = null */
-			_putchar((val % 10) + '0');
-			if (col < 9)
+			_putchar(44);
+			_putchar(32);
+			if (val <= 9)
 			{
-				_putchar(44);
 				_putchar(32);
-				_putchar(32);
+				_putchar(val + '0');
+			}
+			else
+			{
+				_putchar((val / 10) + '0');
+				_putchar((val % 10) + '0');
 			}
 		}
 	_putchar('\n');

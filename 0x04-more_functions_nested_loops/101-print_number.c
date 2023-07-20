5f5f67a4aace122c9190364df61fef2fbb9e06cc
Wahab Michael <65517681+mycrochip@users.noreply.github.com>
2023-07-20 13:57:29 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,12 +11,12 @@ void print_number(int n)
 
         if (num < 0)
         {
-                _putchar('-');
+                _putchar(45);
                 num *= -1;
         }
 
         if ((num / 10) > 0)
                 print_number(num / 10);
 
-        _putchar((num % 10) + '0');
+        _putchar((num % 10) + 48);
 }

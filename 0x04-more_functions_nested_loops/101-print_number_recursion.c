4f53464e4f343fa79ab3a9f2e7b0fa12c74cbc03
Wahab Michael <65517681+mycrochip@users.noreply.github.com>
2023-07-20 14:39:58 +0100

    Create 101-print_number_recursion.c
    Failed some test conditions in ALX

---
+++ 0x04-more_functions_nested_loops/101-print_number_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_number - Prints integers using _putchar function
+* @n: The integer to print
+* Return: Nothing
+*/
+void print_number(int n)
+{
+        int num = n;
+
+        if (num < 0)
+        {
+                _putchar(45);
+                num *= -1;
+        }
+
+        if ((num / 10) > 0)
+                print_number(num / 10);
+
+        _putchar((num % 10) + 48);
+}

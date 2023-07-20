89967ab03d526a38d59464a4bf470abad6703915
Wahab Michael <65517681+mycrochip@users.noreply.github.com>
2023-07-20 11:09:52 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,13 +4,9 @@
 * mul - function that multiplies two integers
 * @a: First integer
 * @b: Second integer
-* Return: mul.
+* Return: a * b.
 */
 int mul(int a, int b)
 {
-	int x;
-
-	x = a * b;
-
-	return (x);
+	return (a * b);
 }

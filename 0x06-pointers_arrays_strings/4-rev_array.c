78026d8d26fc55e2e72ba8294534fc63b625abd8
Wahab Michael <65517681+mycrochip@users.noreply.github.com>
2023-07-26 13:14:58 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,11 +8,10 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i;
-	int temp;
+	int i, temp;
 
 	i = 0;
-	while (n - i > 1)
+	while ((n - i > 1) && (i < n / 2))
 	{
 		temp = a[i];
 		a[i++] = a[--n];

4efe289c029e4054b48a203133d0f64afedfa7fd
Wahab Michael <65517681+mycrochip@users.noreply.github.com>
2023-07-27 15:32:18 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,16 +14,16 @@ char *rot13(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 26; j++)
-			{
-				if ((index = j - 13) < 0)
-					index += 26;
-				if (str[i] == rot13a[j])
-					str[i] = rot13a[index];
-				else if (str[i] == rot13b[j])
-					str[i] = rot13b[index];
-				else
-					continue;
-			}
+		{
+			if ((index = j - 13) < 0)
+				index += 26;
+			if (str[i] == rot13a[j])
+				str[i] = rot13a[index];
+			else if (str[i] == rot13b[j])
+				str[i] = rot13b[index];
+			else
+				continue;
+		}
 	}
 	return (str);
 }
